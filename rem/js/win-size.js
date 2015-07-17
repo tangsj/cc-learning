a533bc898d3247b47c4e75730c1c643146df44d9
tangsj <t_fate@163.com>
2015-07-17 22:19:37 +0800

    update

--- rem/js/win-size.js
+++ rem/js/win-size.js
@@ -1,4 +1,4 @@
-!function(doc, win){
+/*!function(doc, win){
   var docEl = doc.documentElement,
     resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize',
     recalc = function () {
@@ -9,5 +9,33 @@
 
   if (!doc.addEventListener) return;
   win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+}(document, window);*/
+
+
+!function(doc, win){
+  var dpr, rem, scale, resizeEvt;
+
+  var docEl = doc.documentElement;
+  var metaEl = doc.querySelector('meta[name="viewport"]');
+
+  //resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize',
+  dpr = win.devicePixelRatio || 1;
+  scale = 1 / dpr;
+
+  recalc = function () {
+    
+    var clientWidth = docEl.clientWidth;
+
+    if (!clientWidth) return;
+    alert(clientWidth + '--' + dpr);
+    docEl.style.fontSize = 64 * (clientWidth * dpr / 640) + 'px';
+
+    metaEl.setAttribute('content', 'width=' + dpr * clientWidth + ',initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no');
+    docEl.setAttribute('data-dpr', dpr);
+  };
+
+  if (!doc.addEventListener) return;
+  win.addEventListener('orientationchange', recalc, false);
   doc.addEventListener('DOMContentLoaded', recalc, false);
 }(document, window);
