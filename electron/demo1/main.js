2336447ac90d3a1abcb6e42a1a9b66f6889128f1
tangsj <t_fate@163.com>
2019-02-16 10:50:42 +0800

    electron test

--- electron/demo1/main.js
+++ electron/demo1/main.js
@@ -1,10 +1,23 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow } = require('electron');
+const Serialport = require('serialport');
 
 var window = null;
 
 app.on('ready', function() {
-  window = new BrowserWindow({width: 1024, height: 600});
-  window.loadURL('https://www.hjlapp.com/admin');
+  window = new BrowserWindow({ 
+    width: 1024, 
+    height: 600, 
+    webPreferences: {
+      devTools: true,
+      nodeIntegration: true,
+    },
+  });
+
+  window.on('closed', () => {
+    window = null
+  });
+
+  // window.webContents.openDevTools()
+
+  window.loadFile('./index.html');
 });
